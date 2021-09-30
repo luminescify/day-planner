043880f06a3b7826b9f989bb4924a4fede5af606
Leah Davis <ileahd96@gmail.com>
2021-09-29 22:35:27 -0400

    Added moment function to change box colors depending on if hour is past, present, or future. Set up local storage and retrieval.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,44 @@
 $(document).ready(function() {
-// On save button click, grab value of description and id of time
+// On save button click, save hour + description to local storage
 $(".saveBtn").on("click", function() {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-    console.log(value);
-    console.log(time);
+    localStorage.setItem(time, value);
+    console.log(time, value);
 })
 
+// Pull content saved in local storage to the text area and keep it there, even on refresh
+$("#hour-9").children("textarea").val(localStorage.getItem("hour-9"));
+$("#hour-10").children("textarea").val(localStorage.getItem("hour-10"));
+$("#hour-11").children("textarea").val(localStorage.getItem("hour-11"));
+$("#hour-12").children("textarea").val(localStorage.getItem("hour-12"));
+$("#hour-13").children("textarea").val(localStorage.getItem("hour-13"));
+$("#hour-14").children("textarea").val(localStorage.getItem("hour-14"));
+$("#hour-15").children("textarea").val(localStorage.getItem("hour-15"));
+$("#hour-16").children("textarea").val(localStorage.getItem("hour-16"));
+$("#hour-17").children("textarea").val(localStorage.getItem("hour-17"));
+
+
+// Sets current date and day of the week in the header
 var currentDate = moment();
 $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
 
+// Updates background color according to time past, present or future
+var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+updateTime();
+
+    function updateTime() {
+        var currentTime = moment().format("H");
+        for(var i = 0; i < timeOfDay.length; i++) {
+    
+            if (parseInt(timeOfDay[i]) > currentTime) {
+                $("#" + timeOfDay[i]).attr("style", "background-color: #58ce7b");
+            } else if (parseInt(timeOfDay[i]) < currentTime) {
+                $("#" + timeOfDay[i]).attr("style", "background-color: lightgray");
+            } else if (parseInt(timeOfDay[i]) == currentTime) {
+                $("#" + timeOfDay[i]).attr("style", "background-color: #fc665e");
+            }
+      }
+    }
 })
